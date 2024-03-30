a96c847134f7e47d6148e96edac9238f7d1d6493
“betiwe” <betiwe@yandex.ru>
2024-03-30 18:41:24 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
-// TODO: write your code here
-class Character {
+export class Character {
   constructor(name, type) {
     this.name = name;
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
+    this.attack = undefined;
+    this.defence = undefined;
     if (typeof name != "string" || name.length < 2 || name.length > 10) {
       throw new Error("Введено неверное название!");
     }
@@ -23,7 +22,7 @@ class Character {
     }
   }
   levelUp() {
-    if (this.health != 0) {
+    if (this.health > 0) {
       this.level++;
       this.attack *= 1.2;
       this.defence *= 1.2;
@@ -31,13 +30,13 @@ class Character {
     }
   }
   damage(points) {
-    if (this.health >= 0) {
+    if (this.health > 0) {
       this.health -= points * (1 - this.defence / 100);
     }
   }
 }
 
-class Bowman extends Character {
+export class Bowman extends Character {
   constructor(name) {
     super(name, "Bowman");
     this.attack = 25;
@@ -45,7 +44,7 @@ class Bowman extends Character {
   }
 }
 
-class Swordsman extends Character {
+export class Swordsman extends Character {
   constructor(name) {
     super(name, "Swordsman");
     this.attack = 40;
@@ -53,7 +52,7 @@ class Swordsman extends Character {
   }
 }
 
-class Magician extends Character {
+export class Magician extends Character {
   constructor(name) {
     super(name, "Magician");
     this.attack = 10;
@@ -61,7 +60,7 @@ class Magician extends Character {
   }
 }
 
-class Undead extends Character {
+export class Undead extends Character {
   constructor(name) {
     super(name, "Undead");
     this.attack = 25;
@@ -69,7 +68,7 @@ class Undead extends Character {
   }
 }
 
-class Zombie extends Character {
+export class Zombie extends Character {
   constructor(name) {
     super(name, "Zombie");
     this.attack = 40;
@@ -77,17 +76,10 @@ class Zombie extends Character {
   }
 }
 
-class Daemon extends Character {
+export class Daemon extends Character {
   constructor(name) {
     super(name, "Daemon");
     this.attack = 10;
     this.defence = 40;
   }
 }
-
-const bowman = new Bowman("Jasper", "Bowman");
-const swordsman = new Swordsman("Gerald", "Swordsman");
-const magician = new Magician("Magnus", "Magician");
-const undead = new Undead("William", "Undead");
-const zombie = new Zombie("Dexter", "Zombie");
-const daemon = new Daemon("Henry", "Daemon");
